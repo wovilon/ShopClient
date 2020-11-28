0d8b36459b4c5a5bccec0029776cbb05cb0c3c86
Vladimir <wwwovilon@gmail.com>
2020-11-28 23:19:44 +0200

    firestore simple data

--- lib/screens/Gallery.dart
+++ lib/screens/Gallery.dart
@@ -1,13 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:shop_client/model/AppState.dart';
-import 'package:provider/provider.dart';
+
 
 class GalleryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<AppState>();
     return Scaffold(
+      appBar: AppBar(title: Text('List of items from FireStore'),
+        backgroundColor: Colors.green,
+      ),
       body: SafeArea(
         child: Container(color: Colors.lightGreenAccent,
           child:
@@ -20,11 +21,21 @@ class GalleryScreen extends StatelessWidget {
 
   Widget _buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
+      stream: Firestore.instance.collection('products').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
-        return Text(snapshot.data.documents[0].data.toString());
+        return ListView.builder(
+          itemCount: snapshot.data.documents.length,
+          itemBuilder: (context, i){
+            return ListTile(
+              title: Text(snapshot.data.documents[i].data.toString()),
+            );
+          },
+        );
+
+
+        //return Text(snapshot.data.documents[i].data.toString());
       },
     );
   }
